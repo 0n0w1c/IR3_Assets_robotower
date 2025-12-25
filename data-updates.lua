7adeb886a179c93ecf8b43d3e383e57f3ce34b64
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2025-12-24 18:09:47 -0600

    Version 0.0.3

--- data-updates.lua
+++ data-updates.lua
@@ -1,2 +1,10 @@
+if not (mods["IndustrialRevolution3Assets1"]
+        and mods["IndustrialRevolution3Assets2"]
+        and mods["IndustrialRevolution3Assets3"]
+        and mods["IndustrialRevolution3Assets4"]
+    ) then
+    return
+end
+
 table.insert(data.raw["technology"]["construction-robotics"].effects, { type = "unlock-recipe", recipe = "robotower" })
 table.insert(data.raw["technology"]["logistic-robotics"].effects, { type = "unlock-recipe", recipe = "robotower" })
--- data.lua
+++ data.lua
@@ -1,3 +1,11 @@
+if not (mods["IndustrialRevolution3Assets1"]
+        and mods["IndustrialRevolution3Assets2"]
+        and mods["IndustrialRevolution3Assets3"]
+        and mods["IndustrialRevolution3Assets4"]
+    ) then
+    return
+end
+
 require("prototypes/entity/robotower")
 require("prototypes/item/robotower")
 require("prototypes/recipe/robotower")
